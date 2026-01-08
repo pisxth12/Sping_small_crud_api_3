382d672a88fdd256cb7f32c79bb45ac56642057f
pisxth12 <pxsxth1214@gmail.com>
2026-01-08 18:41:33 +0700

    small crud with toggle

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.apiResponse.ApiResponse;
 import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.web.bind.annotation.*;
@@ -11,39 +12,65 @@ import java.util.Optional;
 @RequestMapping("/student")
 public class StudentController {
 
+
     private StudentRepository studentRepository;
 
     public StudentController(StudentRepository studentRepository){
         this.studentRepository = studentRepository;
     }
 
-    //Create
     @PostMapping
-    public Student create(@RequestBody Student s){
-        return studentRepository.save(s);
+    public ApiResponse<Student> create(@RequestBody Student s){
+
+        Student saved = studentRepository.save(s);
+
+        return new ApiResponse<>(true, "Student Create success", saved);
     }
 
+
     @GetMapping
-    public List<Student> getAll(){
-        return studentRepository.findAll();
+    public ApiResponse<List<Student>> getALL(){
+        List<Student> list = studentRepository.findAll();
+        return new ApiResponse<>(true, "Get all student success", list);
     }
 
     @GetMapping("/{id}")
-    public Optional<Student> getByID(@PathVariable Long id){
-        return studentRepository.findById(id);
+    public ApiResponse<Student> getById(@PathVariable Long id){
+        Student student = studentRepository.findById(id).orElseThrow(()-> new RuntimeException("Student with id: "+id+"Not Founnd"));
+        return new ApiResponse<>(true, "Get Student success", student);
     }
 
-    //Update
     @PutMapping("/{id}")
-    public Student update(@PathVariable Long id, @RequestBody Student s){
-        s.setId(id);
-        return studentRepository.save(s);
+    public ApiResponse<Student> update(@PathVariable Long id, @RequestBody Student s){
+        Student exist = studentRepository.findById(id).orElseThrow(()->new RuntimeException("Student not found"));
+        exist.setName(s.getName());
+        exist.setAge(s.getAge());
+        Student updated = studentRepository.save(exist);
+        return new ApiResponse<>(true , "Student update successfully", updated);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public ApiResponse<Void> delete(@PathVariable Long id){
         studentRepository.deleteById(id);
+        return new ApiResponse<>(true,"Delete success", null);
     }
+
+
+    @PatchMapping("/{id}/toggle-status")
+    public ApiResponse<Student> toggleStatus(@PathVariable Long id){
+        Student s = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+        s.setActive(!s.getActive());
+
+        Student updated = studentRepository.save(s);
+        String status = updated.getActive() ? "Active" : "Inactive";
+        return new ApiResponse<>(true, "Student status changed to " + status, updated);
+    }
+
+
+
+
+
 
 
 
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,7 +12,53 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private int age;
+
+
+//    @JsonProperty("status")
+    private boolean active = true;
+
+    public Student(Long id, String name, int age, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.active = active;
+    }
+    public Student(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
